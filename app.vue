cb0049f0f001cc444854e61521b5d66e44f90363
Kerctq <islapnah@gmail.com>
2021-03-20 00:14:50 +0200

    7th-class-1st-half

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p><input type="text" v-model="message"></p>
+        <par-cap v-bind:p-text="message"></par-cap>
+    </div>
+</template>
+<script>
+    var ParCap = require("./par-cap.vue")
+
+    module.exports = {
+        data: function() {
+            return {message: "hello, component"}
+        },
+        components: {
+            parCap: ParCap
+        }
+    }
+</script>
---
+++ par-cap.vue
@@ -0,0 +1,16 @@
+<style scoped>
+    div {
+        display: inline-block;
+        background: hotpink;
+    }
+</style>
+
+<template>
+    <div>
+        <p>{{pText.toUpperCase()}}</p>
+    </div>
+</template>
+
+<script>
+    module.exports = {props: ["pText"]}
+</script>
